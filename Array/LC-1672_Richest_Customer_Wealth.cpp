881271f3dfecc07e22b0618b266b6708905905c4
Shakhawat Hossain <shakhawathossain.se@gmail.com>
2022-08-21 12:10:09 +0600

    //TC: O(n^2)

---
+++ Array/LC-1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,59 @@
+//problem link:
+//solve status:
+//learnings:
+
+//TC: O()
+//MC: O()
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+	int maximumWealth(vector<vector<int>> &accounts)
+	{
+		int max_wealth = 0, sum = 0;
+		for (int i = 0; i < accounts.size(); ++i)
+		{
+			for (int j = 0; j < accounts[i].size(); ++j)
+			{
+				sum = accounts[i][j] + sum;
+
+			}
+			max_wealth = max(sum, max_wealth);
+			sum = 0;
+		}
+
+		return max_wealth;
+	}
+
+
+	int maximumWealth_efficient(vector<vector<int>>& a, int m = 0) {
+		for (auto i : a)
+			m = max(m, accumulate(i.begin(), i.end(), 0));
+		return m;
+	}
+
+
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+//[1,5],[7,3],[3,5]
+//{{1,5},{7,3},{3,5}}
+//[2,8,7],[7,1,3],[1,9,5]
+	vector<vector<int>> x = {{2, 8, 7}, {7, 1, 3}, {1, 9, 5}};
+
+	Solution a;
+	cout << a.maximumWealth(x) << endl;
+
+
+}
